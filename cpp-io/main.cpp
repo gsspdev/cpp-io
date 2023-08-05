292b7747a204e83a51d11e101c4fa7acb2fe0ce5
gsspdev <dev@gssp.io>
2023-08-04 23:51:34 -0700

    init commit

---
+++ cpp-io/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+
+  // ##OUTPUT#
+  cout << 10 << endl; // returns vector of digits
+            // the character '1' followed by '0'
+
+  int x {10};
+  cout << x; // equivalent
+
+class h(result) {
+  public:
+    char[] result = "result";
+  fn h1(int i) {
+    cout << "the value of i is ";
+    cout << i;
+    cout << '\n';
+    } // out: the value of i is 10
+
+  fn h2(int j)
+  {
+    cout << "the value of i is " << j << '\n';
+  } // hefactored from h1
+  private:
+  printf("%s", result);
+
+}
+
+h1(10);
+h2(10);
+  // ##INPUT#
+
+  int i;
+  cin >> i; // reads integer into i
+
+  double d;
+  cin << d; // reads double into d
+  // Refactors to...
+
+  int i;
+  double d;
+  cin >> i >> d; // read into i and d
+
+
+  // ##LOG#
+  cout << "this is a log statement" << endl;
+  return 0;
+}
