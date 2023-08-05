cd7681c958943c2e44bd54630d529aa68f6b3d32
gsspdev <dev@gssp.io>
2023-08-04 23:55:46 -0700

    considerably improved code

--- cpp-io/main.cpp
+++ cpp-io/main.cpp
@@ -1,52 +1,40 @@
 #include <iostream>
 
-using std::cout;
-using std::endl;
+using namespace std;
+
+class HelperClass {
+public:
+    string result = "result";
+    void h1(int i) {
+        cout << "The value of i is " << i << '\n';
+    }
+
+    void h2(int j) {
+        cout << "The value of i is " << j << '\n';
+    }
+    
+    // Share this function with direct access not recommended
+    // considering moving this to a getter or similar
+    void printResult() {
+        cout << result << endl;
+    }
+};
 
 int main() {
 
-  // ##OUTPUT#
-  cout << 10 << endl; // returns vector of digits
-            // the character '1' followed by '0'
+  cout << "10" << endl; 
+  int x = 10;
+  cout << x << endl;
 
-  int x {10};
-  cout << x; // equivalent
+  HelperClass helper;
 
-class h(result) {
-  public:
-    char[] result = "result";
-  fn h1(int i) {
-    cout << "the value of i is ";
-    cout << i;
-    cout << '\n';
-    } // out: the value of i is 10
-
-  fn h2(int j)
-  {
-    cout << "the value of i is " << j << '\n';
-  } // hefactored from h1
-  private:
-  printf("%s", result);
-
-}
-
-h1(10);
-h2(10);
-  // ##INPUT#
-
-  int i;
-  cin >> i; // reads integer into i
-
-  double d;
-  cin << d; // reads double into d
-  // Refactors to...
+  helper.h1(x);
+  helper.h2(x);
 
   int i;
   double d;
-  cin >> i >> d; // read into i and d
+  cin >> i >> d;
 
-
-  // ##LOG#
-  cout << "this is a log statement" << endl;
+  cout << "This is a log statement" << endl;
   return 0;
 }
